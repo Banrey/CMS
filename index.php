fea51165d45b1676dff212c14561b769260c22d0
Banrey <114892207+Banrey@users.noreply.github.com>
2024-10-30 11:28:49 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="styles.css"/>
+</head>
+<body>
+    <div id="wrapper">
+        <div class="header">
+
+</div>
+<div class= "wrapper">
+    <div class="left-sidebar">
+        <div class="login-box">
+            <form method="post" action="actions/user_accounts.php">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" placeholder="Username...">
+                <label>Password></label>
+                <input type="text" name="password" class="form-control" placeholder="Password...">
+                <input type="submit" value="login" name="submit">
+                <input type="reset" value="submit">
+            </form>
+            <a href="register.php">Register</a>
+</div>
+</div>
+<div class="container">
+    <h3>Welcome</h3>
+    <a href="index.php">Home</a> / <a href="reg.php">Register</a>
+</div>
+</div>
+</div>
+</body>
+</html>
+
+
